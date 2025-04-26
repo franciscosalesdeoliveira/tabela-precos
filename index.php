648c3a54a798b76150a2e0fce35158c498932e69
FSales <franciscosalesdeoliveira@outlook.com>
2025-04-26 12:02:25 -0300

    ajustes plano de fundo

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ include_once 'connection.php';
 
 <body>
 
-    <div class="container" id="paginainicial"></div>
-    <h1 class="mt-5">Bem-vindo à Tabela de Preços</h1>
-    <p class="mt-3">Clique no botão abaixo para acessar a tabela de preços.</p>
-    <button class="btn btn-primary mt-2" color="white"><a class="links" target="_blank" href="tabela_precos.php">Tabela de Preços</a></button>
-
-    <button class="btn btn-primary mt-2" color="white"><a class="links" target="_blank" href="cadastro_grupos.php">Cadastro de Grupos</a></button>
-
-    <button class="btn btn-primary mt-2" color="white"><a class="links" target="_blank" href="cadastro_produtos.php">Cadastro de Produtos</a></button>
-    </div>
+    <main>
+        <div class="container col-12" id="paginainicial">
+            <h1 class="mt-5 ">Bem-vindo à Tabela de Preços</h1>
+            <p class="mt-3 botao text-center">Clique nos botões abaixo para navegar pela Tabela de Preços.</p>
+            <button class="btn btn-primary mt-2" color="white"><a class="links" target="_blank" href="tabela_precos.php">Tabela de Preços</a></button>
+            <button class="btn btn-primary mt-2 " color="white"><a class="links" target="_blank" href="cadastro_grupos.php">Cadastro de Grupos</a></button>
+            <button class="btn btn-primary mt-2 " color="white"><a class="links" target="_blank" href="cadastro_produtos.php">Cadastro de Produtos</a></button>
+        </div>
+    </main>
 
 </body>
 
